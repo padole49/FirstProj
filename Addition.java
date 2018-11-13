45bc20f6a86c5a78a62157f42ebbeebb96cf84d9
padole49 <padole49@gmail.com>
2018-11-13 20:32:13 +0530

    Java program

---
+++ Addition.java
@@ -0,0 +1,57 @@
+class Addition
+{
+int res;
+	public static void main(String args[]){
+		int a;
+		int b;
+		a = 10;
+		b = 20;
+		int res  = a+b;
+		System.out.println("RESULT : "+res);
+}
+int result(int i,int j){
+return i+j;
+}
+int bigNumber(int a,int b){
+if (a>b)
+  return a;
+else
+  return b;
+}
+
+String positive(int a){
+if (a>0)
+{System.out.println("number is positive");}
+else
+{System.out.println("number is negative");}
+}
+
+String oddEven(int a){
+if (a%2 == 0)
+{System.out.println("number is even");}
+else
+{System.out.println("number is odd");}
+}
+
+int max(int a,int d,int c){
+           if(a>b && a>c){
+                return a;
+           }
+           else if(b>c){
+                return b;
+           }
+           else {
+                return c;
+           }
+     }
+
+
+for (int i=0;i<=10;i++)
+{ 
+System.out.println(i);
+}
+
+
+
+
+}
